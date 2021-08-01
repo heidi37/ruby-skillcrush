35987e2f49946d74657425b171abeba15bdcb90b
Heidi Fryzell <hfryzell@gmail.com>
2021-08-01 16:53:34 -0400

    refactored first object with accessors

--- ruby_challenges/create-class.rb
+++ ruby_challenges/create-class.rb
@@ -1,40 +1,13 @@
 class Art
 
-    def set_title=(title)
-        @title=title
-    end
+    attr_accessor :title, :artist, :year
 
-    def get_title
-        return @title
-    end
-
-    def set_artist=(artist_name)
-        @artist=artist_name
-    end
-
-    def get_artist
-        return @artist
-    end
-
-    def set_year=(year)
-        @year=year
-    end
-
-    def get_year
-        return @year
-    end
 
 end
 
 class Music < Art
 
-    def set_genre=(genre)
-        @genre=genre
-    end
-
-    def get_genre
-        return @genre
-    end
+    attr_accessor :genre,
 
     def words
         return "Here we are."
@@ -44,15 +17,15 @@ end
 
 
 my_music = Music.new
-my_music.set_title = "Beautiful Mess"
-title = my_music.get_title
-my_music.set_artist = "Jason Mraz"
-artist_name=my_music.get_artist
-my_music.set_year = 2009
-year = my_music.get_year
+my_music.title = "Beautiful Mess"
+title = my_music.title
+my_music.artist = "Jason Mraz"
+artist_name=my_music.artist
+my_music.year = 2009
+year = my_music.year
 
-my_music.set_genre = "Pop"
-genre = my_music.get_genre
+my_music.genre = "Pop"
+genre = my_music.genre
 
 puts "#{title} by #{artist_name} was released in #{year}, is #{genre} and it rocks!!! #{my_music.words}"
 
